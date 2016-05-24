0436938f58f7ade8e05eaf27cc54766ada01efbe
Basile Vu <basile.vu@gmail.com>
2016-05-24 10:22:23 +0200

    Ingredient and Director

---
+++ mcr_builder/src/director/Director.java
@@ -0,0 +1,7 @@
+package director;
+
+/**
+ * Created by Basile Vu on 24.05.2016.
+ */
+public class Director {
+}
---
+++ mcr_builder/src/ingredient/Ingredient.java
@@ -0,0 +1,7 @@
+package ingredient;
+
+/**
+ * Created by Basile Vu on 24.05.2016.
+ */
+public class Ingredient {
+}
